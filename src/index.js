6b89972f66f3a384470c7c6f58c52e33ab9f460e
CarlaMelo <jamislania@gmail.com>
2023-01-28 14:13:27 -0300

    funcoes e estrutura html

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-import validator from './validator.js'; 
+import validator from './validator.js';
+const botao = document.getElementById("validar");
+function validandmask() {
+    let creditCardNumber = document.getElementById("entradanumero").value;
+    let valid = validator.isValid(creditCardNumber)
+    let numberMask = validator.maskify(creditCardNumber)
+    let mensagemResultado = document.getElementById("mensagem")
 
-let creditCardNumber="0123456789"; 
+    if (creditCardNumber==="") {
+         return mensagemResultado.textContent = "Digite o número do Cartão"
 
-console.log(validator.isValid(creditCardNumber));
+    }
+    if (valid) {
+        mensagemResultado.textContent = numberMask + "Cartão Válido"
+
+    } else {
+         mensagemResultado.textContent = "Cartão Inválido"
+
+    }
+ document.getElementById("entradanumero").value = ""
+}
+
+botao.addEventListener("click", validandmask);
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,33 +1,51 @@
 const validator = {
 
-  isValid: function(creditCardNumber) { 
+  isValid: function (creditCardNumber) {
 
-    let cardNumberArray = Array.from(creditCardNumber) 
-    console.log(cardNumberArray) 
+    let cardNumberArray = Array.from(creditCardNumber)
+    const reversed = cardNumberArray.reverse()
+    let totalSum = 0
 
-    const reversed =cardNumberArray.reverse() 
-    let somaMaiorQueNove = 0 
-    console.log(reversed) 
-    for(let index=0; index < reversed.length; index++) { 
-      console.log("oi") 
-      
-      if (index % 2 != 0){ 
+    for (let index = 0; index < reversed.length; index++) {
 
+      if (index % 2 != 0) {
         reversed[index] = (Number(reversed[index])) * 2
-        console.log("2x:", reversed[index]) 
 
-        if (reversed[index] >9) {
-          (reversed[index]) = (reversed[index] -9)
-          console.log(reversed[index]) 
-          somaMaiorQueNove = somaMaiorQueNove + Number(reversed[index]) } 
-          
-          //if(reversed[index] <= 9) {
-            //(reversed[index]) + (reversed[index])
-           // console.log()
+        if (reversed[index] > 9) {
+          (reversed[index]) = (reversed[index] - 9)
 
-            
-          };
+          totalSum = totalSum + Number(reversed[index])
+
+        } else {
+          totalSum = totalSum + Number(reversed[index])
         };
+
+      } else {
+        totalSum = totalSum + Number(reversed[index])
       }
     }
-    export default validator;
+    if (totalSum % 10 === 0) {
+      return true
+    } else {
+      return false
+    }
+
+  },
+
+  maskify: function (number) {
+    const lastFourNumber = 4
+    let mask = ""
+    for (let index = 0; index < number.length; index++) {
+      if (index >= ((number.length) - lastFourNumber)) {
+        mask = mask + number.charAt(index)
+      } else {
+        mask = mask + "#"
+      }
+    }
+    return mask
+  }
+
+}
+
+export default validator;
+
