41cb67b02e3d332b6585cdcacaf3dc942cf73dd8
CarlaMelo <jamislania@gmail.com>
2023-01-30 20:03:07 -0300

    meu proj atualizado

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 import validator from './validator.js';
 const botao = document.getElementById("validar");
 function validandmask() {
-    let creditCardNumber = document.getElementById("entradanumero").value;
-    let valid = validator.isValid(creditCardNumber)
-    let numberMask = validator.maskify(creditCardNumber)
-    let mensagemResultado = document.getElementById("mensagem")
+  let creditCardNumber = document.getElementById("entradanumero").value;
+  let valid = validator.isValid(creditCardNumber)
+  let numberMask = validator.maskify(creditCardNumber)
+  let mensagemResultado = document.getElementById("mensagem")
 
-    if (creditCardNumber==="") {
-         return mensagemResultado.textContent = "Digite o número do Cartão"
+  if (creditCardNumber === "") {
+    return mensagemResultado.textContent = "Digite o número do Cartão"
 
-    }
-    if (valid) {
-        mensagemResultado.textContent = numberMask + "Cartão Válido"
+  }
+  if (valid) {
+    mensagemResultado.textContent = numberMask + "Cartão Válido"
 
-    } else {
-         mensagemResultado.textContent = "Cartão Inválido"
+  } else {
+    mensagemResultado.textContent = "Cartão Inválido"
 
-    }
- document.getElementById("entradanumero").value = ""
+  }
+  document.getElementById("entradanumero").value = ""
 }
 
 botao.addEventListener("click", validandmask);
